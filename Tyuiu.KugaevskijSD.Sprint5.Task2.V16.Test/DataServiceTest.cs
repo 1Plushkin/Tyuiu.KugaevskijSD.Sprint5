4d97b6ae135ad272a397eb72181bc1ef85b37d14
1Plushkin <Plushkin001@mail.ru>
2025-11-25 19:24:12 +0500

    2

---
+++ Tyuiu.KugaevskijSD.Sprint5.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KugaevskijSD.Sprint5.Task2.V16.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint5.Task2.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\1Plushkin\source\repos\Tyuiu.KugaevskijSD.Sprint5\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
