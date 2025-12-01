170008b311562ccd16bf8d8e15d431ebb94651eb
1Plushkin <Plushkin001@mail.ru>
2025-12-01 18:57:19 +0500

    4

---
+++ Tyuiu.KugaevskijSD.Sprint5.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.KugaevskijSD.Sprint5.Task4.V1.Lib;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.KugaevskijSD.Sprint5.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine(@"C:\Users\1Plushkin\source\repos\Sprint5Task4\Sprint5Task4\InPutDataFileTask4V1");
+            FileInfo fileInfo = new FileInfo(path);
+            double wait = -3.680;
+            double res = ds.LoadFromDataFile(path);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint5.Task4.V1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint5.Task4.V1.Lib;
+
+namespace Tyuiu.KugaevskijSD.Sprint5.Task4.V1;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("*****************************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+        Console.WriteLine("*****************************************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask4V1.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("*****************************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+        Console.WriteLine("*****************************************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
