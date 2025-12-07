c4738c1b222c2eb6eda481611ae155a8532c3338
1Plushkin <Plushkin001@mail.ru>
2025-12-07 19:50:48 +0500

    7

---
+++ Tyuiu.KugaevskijSD.Sprint5.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KugaevskijSD.Sprint5.Task7.V23.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint5.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path1 = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+            string path = ds.LoadDataAndSave(path1);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint5.Task7.V23.Lib;
+
+namespace Tyuiu.KugaevskijSD.Sprint5.Task7.V23;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Исходные данные:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+        string pathSaveFile = @"C:\Users\1Plushkin\source\repos\Sprint5Task7\Sprint5Task7\InPutDataFileTask7V23";
+
+
+        Console.WriteLine("Данные находяться в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Находится в файле : ");
+        pathSaveFile = ds.LoadDataAndSave(path);
+        Console.WriteLine(pathSaveFile);
+        Console.ReadKey(); ;
+    }
+}
