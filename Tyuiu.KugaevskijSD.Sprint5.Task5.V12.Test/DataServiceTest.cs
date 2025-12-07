92b378ede2a28d328965d13795169dca88ca0877
1Plushkin <Plushkin001@mail.ru>
2025-12-07 16:07:08 +0500

    5

---
+++ Tyuiu.KugaevskijSD.Sprint5.Task5.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KugaevskijSD.Sprint5.Task5.V12.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint5.Task5.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\1Plushkin\source\repos\Sprint5Task5\Sprint5Task5\InPutDataFileTask5V12";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint5.Task5.V12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint5.Task5.V12.Lib;
+
+namespace Tyuiu.KugaevskijSD.Sprint5.Task5.V12;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Кугаевский С. Д. | АСОиУб-25-1";
+
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("- Спринт #5                                         -");
+        Console.WriteLine("- Тема: Чтение набора данных из текстового файла    -");
+        Console.WriteLine("- Задание #5                                        -");
+        Console.WriteLine("- Вариант #12                                       -");
+        Console.WriteLine("- Выполнил: Кугаевский С. Д. | АСОиУб-25-1          -");
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("- УСЛОВИЕ:                                          -");
+        Console.WriteLine("- Найти разницу между суммой всех положительных и   -");
+        Console.WriteLine("- отрицательных целых чисел в файле.Полученный      -");
+        Console.WriteLine("- результат вывести на консоль.                     -");
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("- ИСХОДНЫЕ ДАННЫЕ:                                  -");
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("- РЕЗУЛЬТАТ:                                        -");
+        Console.WriteLine("-----------------------------------------------------");
+        string path = @"C:\DataSprint5\InPutDataFileTask5V12.txt";
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Результат: " + res);
+        Console.ReadLine();
+    }
+}
